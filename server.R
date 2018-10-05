78af8d00d258c3022d65943e51b984b8beba90f9
Gabriel Bessa <gabrielvcbessa@gmail.com>
2018-10-05 17:13:20 -0300

    Added neighborhoods geojson

--- server.R
+++ server.R
@@ -5,13 +5,42 @@ library(geojsonio)
 library(ggplot2)
 library(dplyr)
 
-df <- fread('./viva_real.csv', encoding = 'UTF-8')
+get_color <- function (d) {
+  case_when(d > 1000 ~ '#800026',
+            d > 500  ~ '#BD0026',
+            d > 200  ~ '#E31A1C',
+            d > 100  ~ '#FC4E2A',
+            d > 50   ~ '#FD8D3C',
+            d > 20   ~ '#FEB24C',
+            d > 10   ~ '#FED976',
+            TRUE     ~ '#FFEDA0')
+}
 
-bh.geojson <- geojson_read('bh.geojson', what = 'sp')
+bh.data <- fread('viva_real_bh.csv', encoding = 'UTF-8')
+
+bh.data <- bh.data[!duplicated(bh.data$id), ]
+iconv(bh.neigh$neighborhood,from="UTF-8",to="ASCII//TRANSLIT")
+bh.neigh <- bh.data %>% 
+  group_by(neighborhood) %>% 
+  summarise(properties = n())
+
+bh.neigh <- transform(bh.neigh, 
+                      neighborhood = iconv(tolower(neighborhood), 
+                                           from = 'UTF-8',
+                                           to = 'ASCII//TRANSLIT'))
+
+bh.geojson <- geojson_read('./bh_neigh.geojson', 
+                           what = 'sp', 
+                           encoding = 'UTF-8')
+
+bh.geojson@data <- transform(bh.geojson@data, 
+                             NOME = iconv(tolower(NOME), 
+                                          from = 'UTF-8',
+                                          to = 'ASCII//TRANSLIT'))
 
 ui <- navbarPage(
   'Dashboard', id = 'nav',
-  tabPanel('Map',
+  tabPanel('Neighborhoods',
            tags$head(
              includeScript('reset_zoom.js'),
              includeCSS('styles.css')),
@@ -26,27 +55,21 @@ ui <- navbarPage(
                              width = 330, height = 'auto',
                              
                              textInput(label = 'Search for a place',
-                                       inputId = 'place-search'),
-                             
-                             plotOutput('histogram', height = 200)))))
+                                       inputId = 'place-search')))))
 
 server <- function(input, output, session) {
   output$properties_map <- renderLeaflet({
-    leaflet(data = bh.geojson) %>% 
+    leaflet(bh.geojson) %>% 
       addTiles() %>% 
       addPolygons(fillColor = '#6692CC',
                   color = '#6692CC',
                   fillOpacity = .1,
-                  weight = 2) %>% 
-      addMarkers(data = df, 
-                 clusterOptions = markerClusterOptions()) %>% 
+                  weight = 1,
+                  highlightOptions = highlightOptions(
+                    weight = 2,
+                    bringToFront = TRUE)) %>% 
       addProviderTiles(providers$CartoDB.Positron)
   })
-  
-  output$histogram <- renderPlot(
-    ggplot(df, aes(rental_price)) +
-      geom_histogram()
-  )
 }
 
 shinyApp(ui, server)
